c83e257c8c86ed2554d79a76dddd1eb8fd854f4a
MagSal <magdalena.salomon@outlook.at>
2025-12-10 18:45:48 +0100

    feature #3 Kostenlogik

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -211,33 +211,37 @@ export default function App() {
     // --- BERECHNUNG DER SCHULDEN ---
     const calculateBalances = (): Balance[] => {
         const balances: Record<string, number> = {};
-
+        //für jeden Tn wird ein Saldo erstellt
         participants.forEach((p) => { balances[p.id] = 0; });
 
+        //jede Ausgabe wird durchgegangen
         expenses.forEach((expense) => {
+            //Wer beteiligt ist an dieser Ausgabe
             const validSplitIds = expense.splitBetween.filter(id =>
                 participants.some(p => p.id === id)
             );
 
             if(validSplitIds.length === 0) return;
 
+            //der Anteil wird berechnet also allgemein
             const shareAmount = expense.amount / validSplitIds.length;
 
+            //Der was bezahlt hat bekommt es gut geschrieben
             if (balances[expense.paidBy] !== undefined) {
                 balances[expense.paidBy] += expense.amount;
             }
-
+            //Eigener Anteil wird jedoch wieder abgezogen
             validSplitIds.forEach((personId) => {
                 if (balances[personId] !== undefined) {
                     balances[personId] -= shareAmount;
                 }
             });
         });
-
+        //Liste für Schuldner erzeugen
         const debtors = Object.entries(balances)
             .filter(([_, amount]) => amount < -0.01)
             .sort((a, b) => a[1] - b[1]);
-
+        //Liste mit neg Saldo erzeugen
         const creditors = Object.entries(balances)
             .filter(([_, amount]) => amount > 0.01)
             .sort((a, b) => b[1] - a[1]);
@@ -245,7 +249,7 @@ export default function App() {
         const result: Balance[] = [];
         let i = 0;
         let j = 0;
-
+        //Vergleich von Schuldner und Gläubiger - entweder Schuldner zahlt alles was er schuldet oder alles, was der Gläubiger bekommen soll je nachdem was kleiner ist
         while (i < debtors.length && j < creditors.length) {
             const [debtorId, debtAmount] = debtors[i];
             const [creditorId, creditAmount] = creditors[j];
@@ -255,6 +259,7 @@ export default function App() {
                 const debtorName = participants.find(p => p.id === debtorId)?.name || debtorId;
                 const creditorName = participants.find(p => p.id === creditorId)?.name || creditorId;
 
+                //"A zahlt B zb. 20 Euro"
                 result.push({
                     from: debtorName,
                     to: creditorName,
@@ -262,6 +267,7 @@ export default function App() {
                 });
             }
 
+            //Schulden ausgleichen
             debtors[i] = [debtorId, debtAmount + amount];
             creditors[j] = [creditorId, creditAmount - amount];
 
